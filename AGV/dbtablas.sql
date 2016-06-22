4028454d5a0373474157c3a7b7943c448be397de
jaimeld <jai.lorca@alumnos.duoc.cl>
2016-06-21 23:36:44 -0300

    Modelo corregido, llave foránea con problemas

--- AGV/dbtablas.sql
+++ AGV/dbtablas.sql
@@ -1,30 +1,149 @@
-CREATE TABLE `usuario` (
-   `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
-  `apellido` varchar(100) COLLATE utf8_bin NOT NULL,
-  `rut` varchar(100) COLLATE utf8_bin NOT NULL,
-  `usuario` varchar(20) COLLATE utf8_bin NOT NULL,
-  `clave` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-CREATE TABLE `producto` (
-  `codigo` varchar(100) COLLATE utf8_bin NOT NULL
-   `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
-  `detalle` varchar(100) COLLATE utf8_bin NOT NULL,
-  `categoria` varchar(100) COLLATE utf8_bin NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-CREATE TABLE `categoria` (
-  `codigo` varchar(100) COLLATE utf8_bin NOT NULL
-   `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
-  `descripcion` varchar(100) COLLATE utf8_bin NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-CREATE TABLE `producto_stock` (
-  `codigo` varchar(100) COLLATE utf8_bin NOT NULL
-   `cantidad` bigint(20) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-CREATE TABLE `boleta` (
-  `codigo` varchar(100) COLLATE utf8_bin NOT NULL
-   `fecha` varchar(100) COLLATE utf8_bin NOT NULL,
-   `producto` varchar(100) COLLATE utf8_bin NOT NULL,
-   `total` bigint(20) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `codigoUsuario` VARCHAR(45) NOT NULL,
+  `nombreUsuario` VARCHAR(45) NOT NULL,
+  `clave` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `mydb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `codigoUsuario` VARCHAR(45) NOT NULL,
+  `nombreUsuario` VARCHAR(45) NOT NULL,
+  `clave` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`producto` (
+  `idproducto` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(45) NOT NULL,
+  `categoria` INT NOT NULL,
+  `precio` INT NOT NULL,
+  PRIMARY KEY (`idproducto`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cliente` (
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(45) NOT NULL,
+  `fechaNacimiento` VARCHAR(45) NOT NULL,
+  `telefono` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `clave` VARCHAR(45) NOT NULL,
+  `idcliente` INT NOT NULL,
+  PRIMARY KEY (`idcliente`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`categoria` (
+  `idcategoria` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(45) NOT NULL,
+  INDEX `categoria_idx` (`idcategoria` ASC),
+  CONSTRAINT `categoria`
+    FOREIGN KEY (`idcategoria`)
+    REFERENCES `mydb`.`producto` (`idproducto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`productoStock`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`productoStock` (
+  `idproductoStock` INT NOT NULL,
+  `stock` INT NOT NULL,
+  `fecha` DATE NOT NULL,
+  PRIMARY KEY (`idproductoStock`),
+  CONSTRAINT `prod`
+    FOREIGN KEY (`idproductoStock`)
+    REFERENCES `mydb`.`producto` (`idproducto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`detallePedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`detallePedido` (
+  `idCliente` INT NOT NULL,
+  `fec_solicitud` VARCHAR(45) NOT NULL,
+  `fec_entrega` VARCHAR(45) NOT NULL,
+  `nroPedido` VARCHAR(45) NOT NULL,
+  `descuento` INT NULL,
+  `precio` INT NOT NULL,
+  `idproducto` INT NOT NULL,
+  `idpedido` INT NOT NULL,
+  INDEX `producto_idx` (`idproducto` ASC),
+  PRIMARY KEY (`idpedido`, `idCliente`),
+  INDEX `cliente_idx` (`idCliente` ASC),
+  CONSTRAINT `prod`
+    FOREIGN KEY (`idproducto`)
+    REFERENCES `mydb`.`producto` (`idproducto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `cliente`
+    FOREIGN KEY (`idCliente`)
+    REFERENCES `mydb`.`cliente` (`idcliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`pedido` (
+  `idpedido` INT NOT NULL,
+  `descripcionProducto` VARCHAR(45) NOT NULL,
+  `precio` VARCHAR(45) NOT NULL,
+  `cantidad` INT NULL,
+  `total` VARCHAR(45) NULL,
+  `idcliente` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idpedido`),
+  CONSTRAINT `detalle`
+    FOREIGN KEY (`idpedido`)
+    REFERENCES `mydb`.`detallePedido` (`idpedido`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
